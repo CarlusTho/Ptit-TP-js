a96c93d86f4cd208a4f577e76049be1b747bec3f
tcar <thomasulrac@gmail.com>
2022-03-14 14:31:19 +0100

    👌 IMPROVE: Code césar logique implementée

--- Caesar.js
+++ Caesar.js
@@ -3,7 +3,31 @@ let chaine = "mh vxlv wkrpdv";
 function decoder(chaine)
 {
     let key = 0;
-    for (key = 0; key = 26; key++ )
+    
+    for (key = 0; key = 26; key++)
+    {
+        let res ='';
+        for (let i = 0; i < chaine.length; i++)
+        {
+            //Passe par chaque lettre
+            let char = chaine[i];
+
+            if (char.match(/[a-z]/i))
+            {
+                let code = chaine.charCodeAt(i);
+
+                if ((code >= 65) && (code <= 90))
+                {
+                    char = String.fromCharCode(((code - 65 + key)) % 26) + 65);
+                } else if ((code >= 97) && (code <= 122))
+                {
+                    char = String.fromCharCode(((code - 97 + key) % 26) + 97);
+                }
+
+                res += char;
+        }
+        console.log(res);
+    }
 }
 
 decoder(chaine);
