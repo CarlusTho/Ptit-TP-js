77a3216512e0ff452b4a9cde62864f8dbf38d815
tcar <thomasulrac@gmail.com>
2022-03-14 12:15:20 +0100

    📦 NEW: Ajout suite de Fibonacci

--- AireCercle.js
+++ AireCercle.js
@@ -2,8 +2,8 @@ let r = 3;
 
 function aireCercle (rayon) 
 {
-    let aire = (rayon * rayon * Math.PI)
-    return aire
+    let aire = (rayon * rayon * Math.PI);
+    return aire;
 }
 
 console.log(aireCercle (r));
---
+++ Fibonacci.js
@@ -0,0 +1,19 @@
+let a = 5;      //Nombre d'itérarions
+
+function Fibonacci (ite)
+{
+    let n = 0;
+    let nplusun = 1;
+    let somme;
+    let i = 0;          // Initialisation des itérations
+    for (n = 0; i = ite; i++)
+    {
+        somme = n + nplusun;
+        console.log(somme);
+        n = nplusun;
+        nplusun = somme;
+        i++;
+    }
+}
+
+Fibonacci(a);
