5573fe45bd727a35446e516a10714fc8b605fbaf
tcar <thomasulrac@gmail.com>
2022-03-14 14:59:22 +0100

    👌 IMPROVE: Logique de la date

---
+++ Date.js
@@ -0,0 +1,14 @@
+function dimanche ()
+{
+    let i = 2014;
+    let day = new Date("January 1, ${i} 01:00:00");
+    for (i = 2014; i = 2050; i++)
+    {
+        if (day.getDate = 0)
+        {
+            return (day);
+        }
+    }
+}
+
+console.log(dimanche());
