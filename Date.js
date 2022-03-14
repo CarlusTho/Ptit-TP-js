a7ed294230b557f96b7a743e8d343124465647e6
tcar <thomasulrac@gmail.com>
2022-03-14 15:00:55 +0100

    🐛 FIX: Correction synthaxique date

--- Date.js
+++ Date.js
@@ -1,7 +1,7 @@
 function dimanche ()
 {
     let i = 2014;
-    let day = new Date("January 1, ${i} 01:00:00");
+    let day = new Date(`January 1, ${i} 01:00:00`);
     for (i = 2014; i = 2050; i++)
     {
         if (day.getDate = 0)
